f8604d3cb2016e2f8731afa4d3c325dfefb01b85
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-01-10 12:08:48 -0300

    adds final formal and smooth scroll JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
